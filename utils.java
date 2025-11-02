13a4094f8ef42a515722cd57e41e011f2ab7723b
squid-7 <dm24csb0a22@student.nitw.ac.in>
2025-11-03 01:27:47 +0530

    Create utils.java

---
+++ utils.java
@@ -0,0 +1,9 @@
+public class Utils {
+    // ... your other methods & constants ...
+
+    // For notification payload
+    public static final String NOTIFICATION_TYPE_NEW_MESSAGE = "NEW_MESSAGE";
+    
+    // FCM SERVER KEY (handled securely in prod! put yours here for now)
+    public static final String FCM_SERVER_KEY = "<YOUR_FCM_SERVER_KEY_HERE>";
+}
